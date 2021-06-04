baca5ae9793e6c52ca07340e57cd37498b8fc14f
yasmineibrahim94 <yasmiiina20@gmail.com>
2021-06-04 12:36:46 +0200

    allow label

--- back/app/schemas/recipe.js
+++ back/app/schemas/recipe.js
@@ -11,7 +11,7 @@ const recipeSchema = Joi.object({
     category_id: Joi.number().integer().positive(),
     description: Joi.string().min(20),
     difficulty_id: Joi.number().integer().positive(),
-    label: Joi.string(),
+    label: Joi.string().allow(null),
     mesure_unit: Joi.array().items(Joi.string()),
     allergy_id: Joi.array().items(Joi.number().allow(null)),
     quantity: Joi.array().items(Joi.number())
